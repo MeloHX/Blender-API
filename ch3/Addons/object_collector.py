6e15fb5e537dcdd6b8cbac94fb5c98ff9497e43a
MeloHX <18992628772@163.com>
2023-08-31 22:46:34 +0800

    APi

--- ch3/Addons/object_collector.py
+++ ch3/Addons/object_collector.py
@@ -1,87 +1,68 @@
 
-import bpy
-
 bl_info = {
- "name": "Collector",
- "author": "John Doe",
- "version": (1, 0),
- "blender": (3, 00, 0),
- "description": "Create collections for object types",
- "category": "Object",
+    "name": "Collector",
+    "author": "John Doe",
+    "version": (1, 0),
+    "blender": (3, 00, 0),
+    "description": "Create collections for object types",
+    "category": "Object",
 }
 
 
-def register():
- # this function is called when the add-on is enabled
-  bpy.utils.register_class(OBJECT_OT_collector_types)
-  menu = bpy.types.VIEW3D_MT_object_context_menu
-  menu.append(draw_collector_item)
+import bpy
 
-def unregister():
- # this function is called when the add-on is disabled
-  bpy.utils.unregister_class(OBJECT_OT_collector_types)
-  menu = bpy.types.VIEW3D_MT_object_context_menu
-  menu.remove(draw_collector_item)
-
-"""A class deriving bpy.types.Operators must implement these members:
-• A static string named bl_idname that contains a unique name by which the operator 
-goes internally
-• A static string named bl_label that contains the displayed name of the operator
-• A poll() class method that verifies that the conditions for executing the operator are met 
-and return either True or False
-• An execute() method that runs when the operator is executed, returning a set of possible 
-running states
-• Optionally, a docstring that Blender will display as additional information"""
 
 class OBJECT_OT_collector_types(bpy.types.Operator):
- """Create collections based on objects types"""
- # F3后输入create type 会出现bl_idname的名字，使得blender完成这个插件指定的操作
- bl_idname = "object.pckt_type_collector"
- bl_label = "Create Type Collections"
+    """Create collections based on objects types"""
+    bl_idname = "object.pckt_type_collector"
+    bl_label = "Create Type Collections"
 
- @classmethod
- def poll(cls, context):
-   return len(context.scene.objects) > 0
- 
-#  #  to create the Mesh, Light, and Camera collections and bring each object under each one of them.
-#  def execute(self, context):
-#     mesh_cl = bpy.data.collections.new("Mesh")
-#     light_cl = bpy.data.collections.new("Light")
-#     cam_cl = bpy.data.collections.new("Camera")
-#     context.scene.collection.children.link(mesh_cl)
-#     context.scene.collection.children.link(light_cl)
-#     context.scene.collection.children.link(cam_cl)
-#     for ob in context.scene.objects:
-#       if ob.type == 'MESH':
-#         mesh_cl.objects.link(ob)
-#       elif ob.type == 'LIGHT':
-#         light_cl.objects.link(ob)
-#       elif ob.type == 'CAMERA':
-#         cam_cl.objects.link(ob)
-#     return {'FINISHED'}
- 
-@staticmethod
-def get_collection(name):
- '''Returns the collection named after the given
- argument. If it doesn't exist, a new collection
- is created and linked to the scene'''
- try:
-  return bpy.data.collections[name]
- except KeyError:
-  cl = bpy.data.collections.new(name)
-  bpy.context.scene.collection.children.link(cl)
-  return cl
- 
-def execute(self, context):
-  for ob in context.scene.objects:
-    cl = self.get_collection(ob.type.title())
-    try:
-      cl.objects.link(ob)
-    except RuntimeError:
-      continue
-  return {'FINISHED'}
+    @classmethod
+    def poll(cls, context):
+        return len(context.scene.objects) > 0
+
+    @staticmethod
+    def get_collection(name):
+        """Returns the collection named after the given
+        argument. If it doesn’t exist, a new collection
+        is created and linked to the scene
+
+        Example:
+
+        >>> OBJECT_OT_collector_types.get_collection("Mesh")
+        bpy.data.collections['Mesh']
+        """
+
+        try:
+            return bpy.data.collections[name]
+        except KeyError:
+            cl = bpy.data.collections.new(name)
+            bpy.context.scene.collection.children.link(cl)
+            return cl
+
+    def execute(self, context):
+        for ob in context.scene.objects:
+            cl = self.get_collection(ob.type.title())
+            try:
+                cl.objects.link(ob)
+            except RuntimeError:
+                continue
+
+        return {'FINISHED'}
+
 
 def draw_collector_item(self, context):
- row = self.layout.row()
- row.operator(OBJECT_OT_collector_types.bl_idname)
- 
+    # Menu functions must accept self and context as argument
+    # context is left unused in this case
+    row = self.layout.row()
+    row.operator(OBJECT_OT_collector_types.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(OBJECT_OT_collector_types)
+    bpy.types.VIEW3D_MT_object_context_menu.append(draw_collector_item)
+
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_collector_types)
+    bpy.types.VIEW3D_MT_pose_context_menu.remove(draw_collector_item)
---
+++ ch4/Addons/object_elevator.py
@@ -0,0 +1,105 @@
+
+bl_info = {
+    "name": "Elevator",
+    "author": "John Doe",
+    "version": (1, 0),
+    "blender": (3, 00, 0),
+    "description": "Move objects up to a minimum height",
+    "category": "Object",
+}
+
+
+import bpy
+# 用于引入浮点数
+from bpy.props import FloatProperty
+# 因为我们想要显示一个使用约束的复选框，所以用到Bool Property
+from bpy.props import BoolProperty
+
+from copy import copy
+
+# 用来找到最上层的父类，因为我们希望对父类先做出变换，再处理子类，否则可能会导致子类出错
+def ancestors_count(ob):
+    """Return number of objects up in the hierarchy"""
+    ancestors = 0
+    while ob.parent:
+        ancestors += 1
+        ob = ob.parent
+
+    return ancestors
+
+
+def get_constraint(ob, constr_type, reuse=True):
+    """Return first constraint of given type.
+    If not found, a new one is created"""
+    if reuse:
+        for constr in ob.constraints:
+            if constr.type == constr_type:
+                return constr
+    
+    return ob.constraints.new(constr_type)
+
+
+class OBJECT_OT_elevator(bpy.types.Operator):
+    """Move Objects up to a certain height"""
+    bl_idname = "object.pckt_floor_transform"
+    bl_label = "Elevate Objects"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    floor: FloatProperty(name="Floor", default=0)
+    constr: BoolProperty(name="Constraints", default=False)
+    # 这样做的目的是避免重复约束，即：在循环中，检查可以使用的现有约束。如果没有找到，脚本就创建它
+    reuse: BoolProperty(name="Reuse", default=True)
+
+    @classmethod
+    # 因为作用于对象，所以要检查有无对象
+    def poll(cls, context):
+        return len(bpy.context.selected_objects) > 0
+
+    def execute(self, context):
+        if self.constr:
+            for ob in context.selected_objects:
+                limit = get_constraint(ob, 'LIMIT_LOCATION', self.reuse)
+
+                limit.use_min_z = True
+                limit.min_z = self.floor
+            
+            return {'FINISHED'}
+
+        # affect coordinates directly
+        # sort parent objects first
+        # context. selected_objects being read-only,  we cannot reorder it directly ，we need to copy its content to a list：
+        # Now we can order this list in a way that won’t cause the same object to be moved twice(#copy())
+        selected_objects = copy(context.selected_objects)
+        selected_objects.sort(key=ancestors_count)
+
+        for ob in selected_objects:
+            matrix_world = ob.matrix_world
+            # 在矩阵中判断z的高度是否到达最小高度
+            if matrix_world[2][3] > self.floor:
+                continue    
+
+            matrix_world[2][3] = self.floor
+            # make sure next object matrix will be updated： Since matrix values are not automatically updated during script execution,
+            
+            context.view_layer.update()
+
+        return {'FINISHED'}
+
+
+def draw_elevator_item(self, context):
+    # 将操作符添加到对象右键菜单中;
+    # Menu functions must accept self and context as argument
+    # context is left unused in this case
+    row = self.layout.row()
+    row.operator(OBJECT_OT_elevator.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(OBJECT_OT_elevator)
+    bpy.types.VIEW3D_MT_object_context_menu.append(draw_elevator_item)
+
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_elevator)
+    bpy.types.VIEW3D_MT_object_context_menu.remove(draw_elevator_item)
+    
